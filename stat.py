99ff913ada06a4205515c6edcd1c4845ce73bcf0
Ilya Pashchenko <in4pashchenko@gmail.com>
2013-11-11 11:28:44 +0400

    Added some test of CV class in stat.py

--- stat.py
+++ stat.py
@@ -1,7 +1,8 @@
-#!/usr/bin python
+#!/usr/bin python2
 # -*- coding: utf-8 -*-
 
 from model import Model
+from gains import Absorber
 from new_data import open_fits
 import glob
 import numpy as np
@@ -15,13 +16,26 @@ class Bootstrap(object):
 
     Inputs:
 
+        uncal - uncalibrated uv-data. FITS-file with data not yet
+            self-calibrated.
+        calibs - iterable of self-calibration sequence.
         residuals - instance of Data class. Difference between
         unself-calibrated data and self-calibrated data with gains added.
 
     """
 
-    def __init__(self, residuals, nonparametric=False, split_scans=False):
-        pass
+    def __init__(self, uncal=None, calibs=None, nonparametric=False,
+                 split_scans=False, use_V=True):
+        absorber = Absorber()
+        absorber.absorb(calibs)
+        last_calib = open_fits(calibs[-1])
+        residuals = uncal - absorber * last_calib
+        self.last_calib = last_calib
+        self.residuals = residuals
+
+        if not nonparametric:
+            noise_residuals = residuals.noise(split_scans=split_scans,
+                                              use_V=use_V)
 
     def sample(self, model, outname=None, nonparametric=None, n=100,
                sigma=None):
@@ -32,7 +46,6 @@ class Bootstrap(object):
         pass
 
 
-# TODO: TEST ME!!!
 class CrossValidation(object):
     """
     Class that implements cross-validation analysis of image-plane models.
@@ -50,6 +63,9 @@ class CrossValidation(object):
             Inputs:
 
                 modelfiles - wildcard of file names ~ 'model_0i_0jofN.txt',
+                    where model in 'model_0i_0jofN.txt' file is from modelling
+                    ``0j``-th training sample ('train_0jofN.FITS') with
+                    ``0i``-th model.
 
                 testfiles - wildcard of file names ~ 'test_0jofN.FITS'.
 
@@ -105,3 +121,18 @@ class LnLikelihood(object):
 
     def __call__(self, p):
         pass
+
+    
+if __name__ == '__main__':
+    
+    data = open_fits('PRELAST_CALIB.FITS')
+    cv = CrossValidation(data)
+    test_fits_files = glob.glob('test*.FITS')
+    if not test_fits_files:
+        data.cv(10, 'test')
+        print "Prepairing testing and training samples."
+    else:
+        print "Testing and training samples are ready."
+    res = cv.run(modelcard = 'model_*_**of10.txt', testcard='test_*of10.FITS')    
+    print res
+    

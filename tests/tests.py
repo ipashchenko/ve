38b5cba489feba25692ff47687f85dbd04ad9980
Ilya Pashchenko <in4pashchenko@gmail.com>
2014-03-05 22:35:36 +0400

    Nothing special

--- tests/tests.py
+++ tests/tests.py
@@ -4,8 +4,8 @@
 import numpy as np
 import pyfits as pf
 from unittest import TestCase
-from utils import aips_bintable_fortran_fields_to_dtype_conversion
-from utils import index_of
+from vlbi_errors.utils import (aips_bintable_fortran_fields_to_dtype_conversion,
+                               index_of)
 
 
 class Test_utils(TestCase):
@@ -18,11 +18,18 @@ class Test_utils(TestCase):
 
     def test_index_of(self):
         self.assertEqual(index_of(np.array([1, 2, 3]),
-                                  np.array([1, 63, 2, 2, 4]), issubset=False),
-                         [np.array([0]), np.array([2, 3]), None])
+                                      np.array([1, 63, 2, 2, 4]),
+                                      issubset=False)[0], np.array([0]))
+        self.assertListEqual(list(index_of(np.array([1, 2, 3]),
+                                           np.array([1, 63, 2, 2, 4]),
+                                           issubset=False)[1]),
+                             list(np.array([2, 3])))
+        self.assertIsNone(index_of(np.array([1, 2, 3]),
+                                   np.array([1, 63, 2, 2, 4]),
+                                   issubset=False)[2])
         with self.assertRaises(AssertionError):
             index_of(np.array([1, 2, 3]), np.array([1, 63, 2, 2, 4]))
 
-    def test_change_shape(self):
-        data = pf.open('PRELAST_CALIB')[0]
+    #def test_change_shape(self):
+    #    data = pf.open('PRELAST_CALIB')[0]
 

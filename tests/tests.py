1236dbf3c073ba35f2e8b680b4b1c7ad01521d88
Ilya Pashchenko <in4pashchenko@gmail.com>
2014-03-13 22:03:22 +0400

    Added sketches of some tests

--- tests/tests.py
+++ tests/tests.py
@@ -2,10 +2,12 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import glob
 import pyfits as pf
 from unittest import TestCase
 from vlbi_errors.utils import (aips_bintable_fortran_fields_to_dtype_conversion,
                                index_of)
+from vlbi_errors.data_io import IO, PyFitsIO, Groups
 
 
 class Test_utils(TestCase):
@@ -33,3 +35,36 @@ class Test_utils(TestCase):
     #def test_change_shape(self):
     #    data = pf.open('PRELAST_CALIB')[0]
 
+
+class Test_IO(TestCase):
+    def setUp(self):
+        self.groups_uv_file = \
+            '/home/ilya/work/vlbi_errors/fits/1226+023_CALIB_SEQ10.FITS'
+        self.idi_uv_file = None
+        self.im_file = \
+            '/home/ilya/work/vlbi_errors/fits/1226+023_ICLN_SEQ11.FITS'
+        self.io = IO()
+
+    def test_IO(self):
+        self.assertRaises(NotImplementedError, self.io.load())
+        self.assertRaises(NotImplementedError, self.io.save())
+
+    def test_PyFitsIO(self):
+        # TODO: How to compare HDU? Compare all keys in header and parnames
+
+
+class Test_Data(TestCase):
+    def setUp(self):
+        self.self_calibrated_fname =\
+            '/home/ilya/work/vlbi_errors/fits/11226+023_CALIB_SEQ10.FITS'
+        self.image_fname =\
+            '/home/ilya/work/vlbi_errors/fits/1226+023_ICLN_SEQ11.FITS'
+
+class Test_gains(TestCase):
+    def setUp(self):
+        fnames = glob.glob('/home/ilya/work/vlbi_errors/fits/12*CALIB*FITS')
+        fnames.sort(reverse=True)
+        fnames.append('/home/ilya/work/vlbi_errors/fits/1226+023_SPT-C1.FITS')
+        self.calibrated_sequence = fnames
+        self.snvers = [1] * len(self.calibrated_sequence)
+        self.snvers[-1] = 2

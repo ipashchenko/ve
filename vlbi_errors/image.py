9c621f182ac8f177dfbab9b5b1685ea514ee7c7a
Ilya Pashchenko <in4pashchenko@gmail.com>
2014-08-15 11:52:11 +0400

    Added ImageSet.add... methods and __init__

--- vlbi_errors/image.py
+++ vlbi_errors/image.py
@@ -1,5 +1,7 @@
 __author__ = 'ilya'
 
+import glob
+
 
 class Image(object):
     """
@@ -50,7 +52,55 @@ class ImageSet(object):
     """
     Class that implements collection of images.
     """
-    def __init__(self):
-        pass
+    def __init__(self, referenced_image=None, imsize=None, pixsize=None,
+                 beam=None, stokes=None):
+        """
+        :param reference:
+            Instance of ``Image`` class, used for setting parameters of images.
+        """
+        self.images = list()
+        if referenced_image is not None:
+            self.referenced_image = referenced_image
+            self.imsize = referenced_image.imsize
+            self.pixsize = referenced_image.pixsize
+            self.beam = referenced_image.beam
+            self.stokes = referenced_image.stokes
+        elif imsize and pixsize and beam and stokes:
+            self.imsize = imsize
+            self.pixsize = pixsize
+            self.beam = beam
+            self.stokes = stokes
+        else:
+            # Use first added image to initialize reference parameters
+            # (``imsize``, ``pixsize``, ``beam``, ``stokes``)
+            pass
 
+    def add_from_fits(self, wildcard, stokes='I'):
+        """
+        Load images from FITS files.
 
+        :param wildcard:
+            Wildcard used for ``glob.glob`` to select FITS files with images.
+
+        :param stokes (optional):
+        """
+        fnames = glob.glob(wildcard)
+        for fname in fnames:
+            image = Image()
+            image.add_from_fits(fname, stokes=stokes)
+            self.images.append(image)
+
+    def add_from_txt(self, wildcard, stokes='I'):
+        """
+        Load images from text files.
+
+        :param wildcard:
+            Wildcard used for ``glob.glob`` to select txt-files with images.
+
+        :param stokes (optional):
+        """
+        fnames = glob.glob(wildcard)
+        for fname in fnames:
+            image = Image()
+            image.add_from_txt(fname, stokes=stokes)
+            self.images.append(image)

d52edad5e4e4997164498a355185ed29ad83531f
Ilya Pashchenko <in4pashchenko@gmail.com>
2014-09-06 15:39:05 +0400

    Added MEMDeconvolution template

--- vlbi_errors/deconvolution.py
+++ vlbi_errors/deconvolution.py
@@ -7,3 +7,10 @@ class Deconvolution(object):
     """
     def __init__(self, uvdata, *args, **kwargs):
         self.uvdata = uvdata
+
+
+class MEMDeconvolution(Deconvolution):
+    def __init_(self, uvdata, dirty_image, dirty_beam, *args, **kwargs):
+        super(MEMDeconvolution, self).__init__(uvdata, *args, **kwargs)
+        self.dirty_image = dirty_image
+        self.dirty_beam = dirty_beam

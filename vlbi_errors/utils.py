e363aee9043094bb6e103d8d01f2d971e5e4ff86
Ilya Pashchenko <in4pashchenko@gmail.com>
2014-11-07 22:05:51 +0300

    gaussian_beam to utils

--- vlbi_errors/utils.py
+++ vlbi_errors/utils.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import re
+import math
 import numpy as np
 import string
 # from itertools import permutations
@@ -375,6 +376,44 @@ def time_dhms_to_frac(dhmses):
         fractions.append(fraction)
 
     return fractions
+
+
+def gaussianBeam(size_x, bmaj, bmin, bpa, size_y=None):
+    """
+    Generate and return a 2D Gaussian function
+    of dimensions (size_x,size_y).
+
+    See Briggs PhD (Appendix B) for details.
+
+    :param size_x:
+        Size of first dimension [pixels].
+    :param bmaj:
+        Beam major axis size [pixels].
+    :param bmin:
+        Beam minor axis size [pixels].
+    :param bpa:
+        Beam positional angle [deg].
+    :param size_y (optional):
+        Size of second dimension. Default is ``size_x``.
+    :return:
+        Numpy.ndarray of size (``size_x``, ``size_y``,).
+    """
+    size_y = size_y or size_x
+    x, y = np.mgrid[-size_x: size_x + 1, -size_y: size_y + 1]
+    # Constructing parameters of gaussian from ``bmaj``, ``bmin``, ``bpa``.
+    a0 = 1. / (0.5 * bmaj) ** 2.
+    c0 = 1. / (0.5 * bmin) ** 2.
+    theta = math.pi * (bpa + 90.) / 180.
+    a = math.log(2) * (a0 * math.cos(theta) ** 2. +
+                       c0 * math.sin(theta) ** 2.)
+    b = (-(c0 - a0) * math.sin(2. * theta)) * math.log(2.)
+    c = math.log(2) * (a0 * math.sin(theta) ** 2. +
+                       c0 * math.cos(theta) ** 2.)
+
+    g = np.exp(-a * x ** 2. - b * x * y - c * y ** 2.)
+    # FIXME: It is already normalized?
+    #return g/g.sum()
+    return g
 
 
 def moments_ext(data, circle, rotate, vheight, estimator=np.median, **kwargs):

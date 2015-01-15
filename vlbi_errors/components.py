99016ec06bf5bcc2fbadb7b82557e59135f6fa4e
Ilya Pashchenko <in4pashchenko@gmail.com>
2015-01-15 18:25:03 +0300

    Move components from model.py to components.py

--- vlbi_errors/components.py
+++ vlbi_errors/components.py
@@ -1,14 +1,13 @@
 import math
 import numpy as np
-from utils import _function_wrapper, mas_to_rad, vcomplex, gaussian
+from vlbi_errors.utils import _function_wrapper, mas_to_rad, vcomplex, gaussian
 
 try:
     import pylab
 except ImportError:
     pylab = None
 
-# TODO: Add MemComponent subclass (array). It is not trivial to reconcile it
-# with Component hierarchy...
+
 class Component(object):
     """
     Basic class that implements single component of model.
@@ -16,6 +15,7 @@ class Component(object):
     def __init__(self):
         self._p = None
         self._parnames = ['flux', 'x', 'y']
+        self._fixed = np.array([False, False, False])
         self._lnprior = dict()
 
     def add_prior(self, **lnprior):
@@ -26,8 +26,9 @@ class Component(object):
             args, kwargs,) where args & kwargs - additional arguments to
             callable. Each callable is called callable(p, *args, **kwargs).
         Example:
-        {'flux': (scipy.stats.norm.logpdf, [mu, s], dict(),),
-        'e': (scipy.stats.beta.logpdf, [alpha, beta], dict(),)}
+        {'flux': (scipy.stats.uniform, [0., 10.], dict(),),
+         'bmaj': (scipy.stats.uniform, [0, 5.], dict(),),
+         'e': (scipy.stats.beta.logpdf, [alpha, beta], dict(),)}
         First key will result in calling: scipy.stats.norm.logpdf(x, mu, s) as
         prior for ``flux`` parameter.
         """
@@ -39,20 +40,20 @@ class Component(object):
             else:
                 raise Exception("Uknown parameter name: " + str(key))
 
-    @property
-    def size(self):
-        return len(self.p)
-
+    # TODO: properties must return only free parameters!
     @property
     def p(self):
         """
         Shortcut for parameters of model.
         """
-        return self._p
+        return self._p[np.logical_not(self._fixed)]
 
     @p.setter
     def p(self, p):
-        self._p = p
+        # print "Setting comp's parameters with ", p
+        # print "Before: ", self._p
+        self._p[np.logical_not(self._fixed)] = p[:]
+        # print "After: ", self._p
 
     def ft(self, uv):
         """
@@ -67,14 +68,14 @@ class Component(object):
 
     def add_to_image_grid(self, image_grid):
         """
-        Add component to instances of ``ImagePlane`` subclasses.
+        Add component to image plane.
 
         :param image_grid:
-            Instance of ``ImagePlane`` subclass.
+            Instance of ``ImagePlane`` class
         """
         raise NotImplementedError("Method must me implemented in subclasses!")
 
-    def uvplot(self, uv, pa=None, style='a&p', sym='.r'):
+    def uvplot(self, uv, style='a&p', sym='.k'):
         """
         Plot FT of component vs uv-radius.
         """
@@ -100,13 +101,14 @@ class Component(object):
 
     @property
     def lnpr(self):
-        print "Calculating lnprior for component ", self
         if not self._lnprior.keys():
             print "No priors specified"
             return 0
         lnprior = list()
         for key, value in self._lnprior.items():
-            lnprior.append(value(self.__getattribute__(key)))
+            # Index of ``key`` in ``_parnames`` the same as in ``_p``
+            par = self._p[self._parnames.index(key)]
+            lnprior.append(value(par))
         return sum(lnprior)
 
 
@@ -114,14 +116,14 @@ class EGComponent(Component):
     """
     Class that implements elliptical gaussian component.
     """
-    def __init__(self, flux, x, y, bmaj, e, bpa):
+    def __init__(self, flux, x, y, bmaj, e, bpa, fixed=None):
         """
         :param flux:
             Flux of component [Jy].
         :param x:
-            X-coordinate of component center [mas].
+            X-coordinate of component phase center [mas].
         :param y:
-            Y-coordinate of component center [mas].
+            Y-coordinate of component phase center [mas].
         :param bmaj:
             Std of component size [mas].
         :param e:
@@ -129,10 +131,20 @@ class EGComponent(Component):
         :param bpa:
             Positional angle of major axis. Angle counted from x-axis of image
             plane counter clockwise [rad].
+        :param fixed optional:
+            If not None then it is iterable of parameter's names that are fixed.
         """
         super(EGComponent, self).__init__()
         self._parnames.extend(['bmaj', 'e', 'bpa'])
-        self._p = [flux, x, y, bmaj, e, bpa]
+        self._fixed = np.concatenate((self._fixed,
+                                      np.array([False, False, False]),))
+        self._p = np.array([flux, x, y, bmaj, e, bpa])
+        self.size = 6
+        if fixed is not None:
+            for par in fixed:
+                if par not in self._parnames:
+                    raise Exception('Uknown parameter ' + str(par) + ' !')
+                self._fixed[self._parnames.index(par)] = True
 
     def ft(self, uv):
         """
@@ -171,14 +183,14 @@ class EGComponent(Component):
             ft(x0,y0) = ft(x0=0,y0=0)*exp(-2*pi*(u*x0+v*y0))
         """
         try:
-            flux, x0, y0, bmaj, e, bpa = self.p
+            flux, x0, y0, bmaj, e, bpa = self._p
         # If we call method inside ``CGComponent``
         except ValueError:
-            flux, x0, y0, bmaj = self.p
+            flux, x0, y0, bmaj = self._p
             e = 1.
             bpa = 0.
 
-        # Convert to radians
+        # There's ONE place to convert them
         x0 *= mas_to_rad
         y0 *= mas_to_rad
         bmaj *= mas_to_rad
@@ -197,8 +209,7 @@ class EGComponent(Component):
         # Calculate the value of FT in point (u,v) for x0=0,y0=0 case using (2)
         k = (4. * a * c - b ** 2.)
         ft = flux * np.exp((4. * math.pi ** 2. / k) * (-c * u ** 2. +
-                                                       b * u * v -
-                                                       a * v ** 2.))
+                                                       b * u * v - a * v ** 2.))
         ft = vcomplex(ft)
         # If x0=!0 or y0=!0 then shift phase accordingly
         if x0 or y0:
@@ -207,62 +218,103 @@ class EGComponent(Component):
 
     def add_to_image_grid(self, image_grid):
         """
-        Add component to instances of ``ImagePlane`` subclasses.
-
-        :param image_grid:
-            Instance of ``ImagePlane`` subclass.
+        Add component to given instance of ``ImagePlane`` class.
         """
-        x, y = image_grid.x, image_grid.y
-        # If in ``EGComponent`` instance
+        # Cellsize
+        dx, dy = image_grid.dx, image_grid.dy
+        # Center of image
+        x_c, y_c = image_grid.x_c, image_grid.y_c
+
+        # Parameters of component
         try:
-            flux, x_comp, y_comp, bmaj, e, bpa = self.p
-        # If in ``CGComponent``instance
+            flux, x0, y0, bmaj, e, bpa = self._p
+        # If we call method inside ``CGComponent``
         except ValueError:
-            flux, x_comp, y_comp, bmaj = self.p
+            flux, x0, y0, bmaj = self._p
             e = 1.
             bpa = 0.
 
-        gauss_flux = gaussian(flux, x_comp, y_comp, bmaj, e, bpa)
-        flux = gauss_flux(x, y)
-        image_grid.image_grid += flux
+        # There's ONE place to convert them
+        x0 *= mas_to_rad
+        y0 *= mas_to_rad
+        bmaj *= mas_to_rad
+
+        # Construct parameter of general gaussian function
+        std_x = bmaj
+        std_y = e * bmaj
+        # Amplitude of gaussian component
+        amp = flux / (2. * math.pi * bmaj ** 2. * e)
+
+        # Create gaussian function of (x, y) with given parameters
+        gaussf = gaussian(amp, x0, y0, std_x, std_y, bpa=bpa)
+
+        # Calculating angular distances of cells from center of component
+        # from cell numbers to relative distances
+        # arrays with elements from 1 to imsize
+        x, y = np.mgrid[1: image_grid.imsize[0] + 1,
+               1: image_grid.imsize[1] + 1]
+        # from -imsize/2 to imsize/2
+        x = x - x_c
+        y = y - y_c
+        # the same in rads
+        x = x * dx
+        y = y * dy
+        # relative to component center
+        x = x - x0
+        y = y - y0
+
+        # Creating grid with component's flux at each cell
+        fluxes = gaussf(x, y)
+
+        # Adding component's flux to image grid
+        image_grid.image_grid += fluxes
 
 
 class CGComponent(EGComponent):
     """
     Class that implements circular gaussian component.
     """
-    def __init__(self, flux, x, y, bmaj):
+    def __init__(self, flux, x, y, bmaj, fixed=None):
         """
         :param flux:
             Flux of component [Jy].
         :param x:
-            X-coordinate of component center [mas].
+            X-coordinate of component phase center [mas].
         :param y:
-            Y-coordinate of component center [mas].
+            Y-coordinate of component phase center [mas].
         :param bmaj:
             Std of component size [mas].
         """
-        super(CGComponent, self).__init__(flux, x, y, bmaj, e=1., bpa=0.)
+        super(CGComponent, self).__init__(flux, x, y, bmaj, e=1., bpa=0.,
+                                          fixed=fixed)
+        self._fixed = self._fixed[:-2]
         self._parnames.remove('e')
         self._parnames.remove('bpa')
-        self._p = [flux, x, y, bmaj]
+        self._p = self._p[:-2]
+        self.size = 4
 
 
 class DeltaComponent(Component):
     """
     Class that implements delta-function component.
     """
-    def __init__(self, flux, x, y):
+    def __init__(self, flux, x, y, fixed=None):
         """
         :param flux:
             Flux of component [Jy].
         :param x:
-            X-coordinate of component center [mas].
+            X-coordinate of component phase center [mas].
         :param y:
-            Y-coordinate of component center [mas].
+            Y-coordinate of component phase center [mas].
         """
         super(DeltaComponent, self).__init__()
-        self._p = [flux, x, y]
+        self._p = np.array([flux, x, y])
+        self.size = 3
+        if fixed is not None:
+            for par in fixed:
+                if par not in self._parnames:
+                    raise Exception('Uknown parameter ' + str(par) + ' !')
+                self._fixed[self._parnames.index(par)] = True
 
     def ft(self, uv):
         """
@@ -273,17 +325,17 @@ class DeltaComponent(Component):
             Numpy array of complex visibilities for specified points of
             uv-plane. Length of the resulting array = length of ``uv`` array.
         """
-        flux, x0, y0 = self.p
+        flux, x0, y0 = self._p
 
-        # Convert to radians
+        # There's ONE place to convert them
         x0 *= mas_to_rad
         y0 *= mas_to_rad
 
         u = uv[:, 0]
         v = uv[:, 1]
-        visibilities = (self.flux * np.exp(2.0 * math.pi * 1j *
-                                           (u[:, np.newaxis] * x0 +
-                                            v[:, np.newaxis] * y0))).sum(axis=1)
+        visibilities = (flux * np.exp(2.0 * math.pi * 1j *
+                                      (u[:, np.newaxis] * x0 +
+                                       v[:, np.newaxis] * y0))).sum(axis=1)
         return visibilities
 
     def add_to_image_grid(self, image_grid):
@@ -293,10 +345,37 @@ class DeltaComponent(Component):
         dx, dy = image_grid.dx, image_grid.dy
         x_c, y_c = image_grid.x_c, image_grid.y_c
 
-        flux, x, y = self.p
-        x_coords = int(round(x / dx))
-        y_coords = int(round(y / dy))
-        # 2 means that x_c & x_coords should be both zero-indexed.
+        flux, x0, y0 = self._p
+
+        # There's ONE place to convert them
+        x0 *= mas_to_rad
+        y0 *= mas_to_rad
+
+        x_coords = int(round(x0 / dx))
+        y_coords = int(round(y0 / dy))
+        # 2 means that x_c & x_coords should be zero-indexed actually both.
         x = x_c + x_coords - 2
         y = y_c + y_coords - 2
         image_grid.image_grid[x, y] += flux
+
+
+# TODO: I need to keep coordinates of pixels for FT!
+class MemComponent(Component):
+    """
+    Class that implements MEM algorithm component (2D-array of flux values).
+    """
+    def __init__(self, image_array):
+        super(MemComponent, self).__init__()
+        self.imsize = np.shape(image_array)
+        self._parnames = [str(i) for i in xrange(self.imsize[0] *
+                                                 self.imsize[1])]
+        self._fixed = np.zeros(len(self._parnames), dtype=bool)
+        self._p = image_array.flatten()
+
+    def add_to_image_grid(self, image_grid):
+        image_grid.image_grid += self._p.reshape(self.imsize)
+
+    def ft(self, uv):
+        pass
+
+

4b4b36fc09f3c71370118c533d3b24b018dcc09b
Ilya Pashchenko <in4pashchenko@gmail.com>
2015-01-16 11:15:55 +0300

    function + some enh.

--- vlbi_errors/beam.py
+++ vlbi_errors/beam.py
@@ -1,15 +1,29 @@
+from scipy import signal
 from vlbi_errors.data_io import get_fits_image_info
+from vlbi_errors.utils import gaussianBeam
 
 
 # * Resolution could depend on position in map or frequency. Should i associate
 # instance of ``Beam`` with each pixel in ``Image`` instance in such cases?
 # * Beam is connected with ``CCModel`` instance naturally by deconvolution
 # process? NO! Delta functions can be used in modelling without CLEAN!
+def create_clean_beam_from_fits(fname):
+    """
+    Create instance of ``CleanBeam`` from FITS-file of CLEAN image.
+    """
+    imsize, pixref, (bmaj, bmin, bpa), pixsize = get_fits_image_info(fname)
+    return CleanBeam(bmaj, bmin, bpa, imsize)
+
 class Beam(object):
     """
     Basic class that represents point spread function.
     """
-    pass
+    def __init__(self):
+        self.image = None
+
+    # Convolve with any object that has ``image`` attribute
+    def convolve(self, image_like):
+        return signal.fftconvolve(self.image, image_like.image, mode='same')
 
 
 class DirtyBeam(Beam):
@@ -23,18 +37,16 @@ class DirtyBeam(Beam):
         pass
 
 
-class CleanBeam(object):
+class CleanBeam(Beam):
     """
     Class that represents central part of point spread function.
     """
-    def __init__(self, bmaj=None, bmin=None, bpa=None):
+    def __init__(self, bmaj=None, bmin=None, bpa=None, size=None):
         self.bmaj = bmaj
         self.bmin = bmin
         self.bpa = bpa
+        self.size = size
+        self.image = gaussianBeam(self.size[0], self.bmaj, self.bmin, self.bpa,
+                                  self.size[1])
 
-    def from_fits(self, fname):
-        imsize, pixref, (bmaj, bmin, bpa), pixsize = get_fits_image_info(fname)
-        self.bmaj = bmaj
-        self.bmin = bmin
-        self.bpa = bpa
 

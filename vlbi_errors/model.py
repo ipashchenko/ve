a676433384445771c4d270566e1d1a1e93c44674
Ilya Pashchenko <in4pashchenko@gmail.com>
2015-01-19 20:20:23 +0300

    make_image use add_to_image

--- vlbi_errors/model.py
+++ vlbi_errors/model.py
@@ -1,9 +1,8 @@
 import math
 import numpy as np
 import scipy as sp
-from utils import degree_to_mas, mas_to_rad, gaussianBeam
-from image import ImageModel
-from scipy import signal
+from utils import degree_to_mas, gaussianBeam
+from new_image import Image
 from data_io import BinTable, get_fits_image_info
 from uv_data import open_fits
 from stats import LnPost
@@ -120,15 +119,15 @@ class Model(object):
     def size(self):
         return len(self.p)
 
-    def add_to_image_grid(self, image_grid):
+    def add_to_image(self, image):
         """
-        Add model to instances of ``ImagePlane`` subclasses.
+        Add model to instances of ``Image`` subclasses.
 
-        :param image_grid:
-            Instance of ``ImagePlane`` subclass.
+        :param image:
+            Instance of ``Image`` subclass.
         """
         for component in self._components:
-            image_grid.add_component(component)
+            image.add_component(component)
 
     def make_image(self, fname=None, imsize=None, bmaj=None, bmin=None,
                    bpa=None, size_x=None, size_y=None):
@@ -157,31 +156,15 @@ class Model(object):
             imsize, pixref, (bmaj, bmin, bpa,), pixsize =\
                 get_fits_image_info(fname)
 
-        # First create image grid with components
-        image_grid = ImageModel(imsize=imsize, pixref=pixref, pixsize=pixsize)
+        # First create ``Image`` instance
+        image = Image(imsize=imsize, pixref=pixref, pixsize=pixsize)
         # Putting model components to image grid
-        self.add_to_image_grid(image_grid)
+        self.add_to_image(image)
 
-        if not size_x:
-            size_x = int(self.imsize[0] / 2.)
-        if not size_y:
-            size_y = int(self.imsize[1] / 2.)
-        # FIXME: this 3 line should be deleted. There're no beam attrs. in self!
-        if not bmaj:
-            bmaj = self.bmaj / abs(self.pixsize[0])
-        if not bmin:
-            bmin = self.bmin / abs(self.pixsize[1])
-        if bpa is None:
-            bpa = self.bpa
-        gaussian_beam = gaussianBeam(size_x, bmaj, bmin, bpa)
-        cc_convolved = signal.fftconvolve(image_grid.image_grid, gaussian_beam,
-                                          mode='same')
-        image = ImageModel()
-        image.add_from_array(cc_convolved, pixsize=self.pixsize, bmaj=self.bmaj,
-                             bmin=self.bmin, bpa=self.bpa)
         return image
 
 
+# TODO: Do i need separate class CCModel???
 # TODO: add ft_all method for speed up
 # TODO: move ``add_cc_from_fits`` to function ``create_ccmodel_from_fits``
 class CCModel(Model):
@@ -204,11 +187,10 @@ class CCModel(Model):
             self.add_component(component)
 
 
-
 if __name__ == "__main__":
 
     # TODO: test with new components.py
-    ## TESTING plotting cc-models on uv-data
+    # # TESTING plotting cc-models on uv-data
     # cc_fits_file = '1038+064.l22.2010_05_21.icn.fits'
     # uv_fits_file = '1038+064.l22.2010_05_21.uvf'
     # ccmodel = CCModel(stokes='I')
@@ -275,8 +257,6 @@ if __name__ == "__main__":
     # # eg.add_to_image_grid(image_grid)
     # # print image_grid.image_grid
 
-
-
     # With sparse RA data
     # TESTING fitting gaussian components to uv-data
     # Load uv-data
@@ -304,8 +284,8 @@ if __name__ == "__main__":
     cov[3, 3] = 0.01
     sampler = emcee.MHSampler(cov, ndim, lnpost)
     pos, prob, state = sampler.run_mcmc(p0, 1000)
-    #sampler.reset()
-    #sampler.run_mcmc(pos, 5000)
+    # sampler.reset()
+    # sampler.run_mcmc(pos, 5000)
     # # image_grid = ImageModel(fname='J0005+3820_S_1998_06_24_fey_map.fits')
     # # print image_grid.dx, image_grid.dy, image_grid.imsize, image_grid.x_c,\
     # #     image_grid.y_c

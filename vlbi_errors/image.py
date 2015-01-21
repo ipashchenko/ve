be1fbb91bf52d70af2c1f3424b781c2378c3bb8a
Ilya Pashchenko <in4pashchenko@gmail.com>
2015-01-21 13:31:50 +0300

    Move new_image.py -> image.py

--- vlbi_errors/image.py
+++ vlbi_errors/image.py
@@ -1,9 +1,8 @@
-import glob
 import numpy as np
 from scipy import signal
-from data_io import get_fits_image_info
-#from model import CCModel
-from utils import gaussianBeam, create_grid
+from utils import create_grid, mask_region, fitgaussian
+from beam import CleanBeam
+from fft_routines import fft_convolve2d
 
 try:
     import pylab
@@ -11,24 +10,16 @@ except ImportError:
     pylab = None
 
 
-# TODO: Add ``remove_*`` methods?
-# TODO: Add operation of comparing of ImageModel instances.
-# TODO: Is this class useless without beam information?
-# TODO: Put ``Beam`` subclass inside (composition). Compare not only image
-# parameters, but also ``Beam`` instances.
-class ImageModel(object):
+class Image(object):
     """
-    Class that represents image grid (array) representation of models.
+    Class that represents images.
     """
-    def __init__(self, fname=None, imsize=None, pixref=None, pixsize=None):
-        if fname:
-            self.from_image(fname)
-        else:
-            self.imsize = imsize
-            self.dx, self.dy = pixsize
-            self.x_c, self.y_c = pixref
+    def __init__(self, imsize=None, pixref=None, pixsize=None):
+        self.imsize = imsize
+        self.dx, self.dy = pixsize
+        self.x_c, self.y_c = pixref
         # Create flux array
-        self.image_grid = np.zeros(self.imsize, dtype=float)
+        self._image = np.zeros(self.imsize, dtype=float)
         # Create coordinate arrays
         x, y = create_grid(self.imsize)
         x = x - self.x_c
@@ -38,17 +29,50 @@ class ImageModel(object):
         self.x = x
         self.y = y
 
-    def convolve_with_beam(self):
+    # TODO: Sometimes we need to add/substract convolved images. So subclasses
+    # should implement property with convolution.
+    @property
+    def image(self):
         """
-        Convolve ``image_grid`` with internal beam.
+        Shorthand for image array.
         """
-        pass
+        return self._image
 
-    def from_image(self, fname):
-        imsize, pixref, (bmaj, bmin, bpa,), pixsize = get_fits_image_info(fname)
-        self.imsize = imsize
-        self.dx, self.dy = pixsize
-        self.x_c, self.y_c = pixref
+    # TODO: Am i need it? Should i compare instances before setting?
+    @image.setter
+    def image(self, image):
+        self._image = image.image
+
+    def __eq__(self, image):
+        """
+        Compares current instance of ``ImageModel`` class with other instance.
+        """
+        return (self.imsize == image.imsize and self.pixsize == image.pixsize)
+
+    def __sum__(self, image):
+        """
+        Sums current instance of ``Image`` class with other instance.
+        """
+        raise NotImplementedError
+
+    def __sub__(self, image):
+        """
+        Substruct from current instance of ``Image`` class other instance.
+        """
+        raise NotImplementedError
+
+    def __div__(self, image):
+        """
+        Divide current instance of ``Image`` class on other instance.
+        """
+        raise NotImplementedError
+
+    # Convolve with any object that has ``image`` attribute
+    def convolve(self, image_like):
+        """
+        Convolve ``Image`` array with image-like instance.
+        """
+        return signal.fftconvolve(self._image, image_like.image, mode='same')
 
     def add_component(self, component):
         component.add_to_image(self)
@@ -63,247 +87,87 @@ class ImageModel(object):
         else:
             raise NotImplementedError
         rvs = rvs.reshape(self.imsize)
-        self.image_grid += rvs
+        self._image += rvs
 
-    # TODO: test it!
-    def __eq__(self, image):
+    # TODO: Should i compare images before?
+    def cross_correlate(self, image, region1=None, region2=None):
         """
-        Compares current instance of ``ImageModel`` class with other instance.
-        """
-        return (self.imsize == image.imsize and self.pixsize == image.pixsize)
-
-    @property
-    def image(self):
-        """
-        Shorthand for image array. If ``image_grid`` is changed then reconvolve
-        with internal instance of ``Beam`` subclass.
-        """
-
-
-
-# TODO: All add operations must be with ``ImageModel`` instances. Here only
-# convolution.
-# TODO: Automatically reconvolve ``ImageModel`` instances after add/sub anything.
-# TODO: Add operation of comparing of Image instances.
-class Image(object):
-    """
-    Class that implements images.
-    :param imsize:
-    :param pixsize:
-    :param beam:
-    :param stokes:
-    """
-    def __init__(self, imsize=None, pixsize=None, pixref=None, bmaj=None,
-                 bmin=None, bpa=None, stokes=None):
-        self.imsize = imsize
-        self.pixsize = pixsize
-        self.pixref = pixref
-        self.bmaj = bmaj
-        self.bmin = bmin
-        self.bpa = bpa
-        self.stokes = stokes
-        # Initialize ``ImageModel`` instance
-
-
-    # TODO: test it!
-    def __eq__(self, image):
-        """
-        Compares current instance of ``Image`` class with other instance.
-        """
-        return (self.image_grid == image.image_grid and self.bmaj == image.bmaj
-                and self.bmin == image.bmin and self.bpa == image.bpa)
-
-    def __sum__(self, image):
-        """
-        Sums current instance of ``Image`` class with other instance.
-        """
-        if self == image:
-            pass
-
-    def add_from_array(self, data, pixsize=None, bmaj=None, bmin=None, bpa=None,
-                       stokes=None):
-        self.image = np.atleast_2d(data)
-        self.imsize = (self._image.shape)
-        self.pixsize = pixsize
-        self.bmaj = bmaj
-        self.bmin = bmin
-        self.bpa = bpa
-
-    def add_from_txt(self, fname, stokes='I'):
-        """
-        Load image from text file.
-
-        :param fname:
-            Text file with image data.
-        :param stokes (optional):
-        """
-        pass
-
-    def add_cc_from_fits(self, fname, stokes='I'):
-        """
-        Load image from FITS file.
-
-        :param fname:
-            FITS file with image data.
-        :param stokes (optional):
-        """
-        model = CCModel()
-        model.add_cc_from_fits(fname, stoke=stokes)
-        image_grid = ImageModel(imsize=self.imsize, pixref=self.pixref,
-                               pixsize=self.pixsize)
-        model.add_to_image(image_grid)
-        gaussian_beam = gaussianBeam(self.imsize[0], self.bmaj, self.bmin,
-                                     self.bpa, self.imsize[1])
-        self.image = signal.fftconvolve(image_grid.image_grid, gaussian_beam,
-                                        mode='same')
-
-    def cross_correlate(self, image, region1=(None, None, None, None),
-                        region2=(None, None, None, None)):
-        """
-        Cross-correlates image with another image.
+        Cross-correlates current instance of ``Image`` with another instance.
 
         Computes normalized cross-correlation of images.
 
         :param image:
             Instance of image class.
-        :param region1:
+        :param region1 (optional):
             Region to EXCLUDE in current instance of ``Image``.
             Or (blc[0], blc[1], trc[0], trc[1],) or (center[0], center[1], r,
-            None,).
-        :param region2:
+            None,). Default ``None``.
+        :param region2 (optional):
             Region to EXCLUDE in ``image``. Or (blc[0], blc[1], trc[0], trc[1],)
-            or (center[0], center[1], r, None,).
+            or (center[0], center[1], r, None,). Default ``None``.
         :return:
             (dx, dy,) tuple of shifts (subpixeled) in each direction.
         """
-        pass
+        if region1 is not None:
+            image1 = mask_region(self.image, region1)
+        if region2 is not None:
+            image2 = mask_region(image.image, region2)
+        # Cross-correlate images
+        shift_array = fft_convolve2d(image1, image2)
+        params = fitgaussian(shift_array)
+        return tuple(params[1: 3])
 
+    # TODO: fix BLC,TRC to display expected behavior. Use data_io.get_image()
     def plot(self, blc=None, trc=None, clim=None, cmap=None):
         """
         Plot image.
+
+        :note:
+            ``blc`` & ``trc`` are AIPS-like (from 1 to ``imsize``). Internally
+            converted to python-like zero-indexing.
         """
         if not pylab:
             raise Exception("Install matplotlib for plotting!")
         if blc or trc:
-            path_to_plot = self._array[blc[0]:trc[0], blc[1]:trc[1]]
-            imgplot = pylab.imshow(path_to_plot)
-            if cmap:
-                try:
-                    imgplot.set_cmap(cmap)
-                except:
-                    # Show wo ``cmap`` set, print availbale ``cmap``s.
-                    pass
-            if clim:
-                # TODO: Warn if ``clim`` is out of range for image.
-                imgplot.set_clim(clim)
-
-
-class ImageSet(object):
-    """
-    Class that implements collection of images.
-    """
-    def __init__(self, referenced_image=None, imsize=None, pixsize=None,
-                 beam=None, stokes=None):
-        """
-        :param reference:
-            Instance of ``Image`` class, used for setting parameters of images.
-        """
-        self.images = list()
-        if referenced_image is not None:
-            self.referenced_image = referenced_image
-            self.imsize = referenced_image.imsize
-            self.pixsize = referenced_image.pixsize
-            self.beam = referenced_image.beam
-            self.stokes = referenced_image.stokes
-        elif imsize and pixsize and beam and stokes:
-            self.imsize = imsize
-            self.pixsize = pixsize
-            self.beam = beam
-            self.stokes = stokes
+            blc = blc or (1, 1,)
+            trc = trc or self.imsize
+            part_to_plot = self.image[blc[0] - 1: trc[0] - 1, blc[1] - 1: trc[1]
+                                      - 1]
         else:
-            # Use first added image to initialize reference parameters
-            # (``imsize``, ``pixsize``, ``beam``, ``stokes``)
-            pass
-        # Keeping image data in 3D-cube.
-        self._3d_array = np.empty((len(self.images), self.pixsize[0],
-                                   self.pixsize[1]))
+            part_to_plot = self.image
+        imgplot = pylab.imshow(part_to_plot)
+        if cmap:
+            try:
+                imgplot.set_cmap(cmap)
+            except:
+                # Show wo ``cmap`` set, print availbale ``cmap``s.
+                pass
+        if clim:
+            # TODO: Warn if ``clim`` is out of range for image.
+            imgplot.set_clim(clim)
 
-    def add_from_fits(self, wildcard, stokes='I'):
+
+class CleanImage(Image):
+    """
+    Class that represents image made using CLEAN algorithm.
+    """
+    def __init__(self, imsize=None, pixref=None, pixsize=None, bmaj=None,
+                 bmin=None, bpa=None):
+        super(CleanImage, self).__init__(imsize, pixref, pixsize)
+        # TODO: What if pixsize has different sizes???
+        self.beam = CleanBeam(bmaj / abs(pixsize[0]), bmin / abs(pixsize[0]),
+                              bpa, imsize)
+
+    @property
+    def image(self):
         """
-        Load images from FITS files.
-
-        :param wildcard:
-            Wildcard used for ``glob.glob`` to select FITS files with images.
-
-        :param stokes (optional):
+        Shorthand for CLEAN image.
         """
-        fnames = glob.glob(wildcard)
-        for fname in fnames:
-            image = ImageModel()
-            image.add_from_fits(fname, stokes=stokes)
-            self.images.append(image)
+        return signal.fftconvolve(self._image, self.beam.image, mode='same')
 
-    def add_from_txt(self, wildcard, stokes='I'):
-        """
-        Load images from text files.
 
-        :param wildcard:
-            Wildcard used for ``glob.glob`` to select txt-files with images.
-
-        :param stokes (optional):
-        """
-        fnames = glob.glob(wildcard)
-        for fname in fnames:
-            image = ImageModel()
-            image.add_from_txt(fname, stokes=stokes)
-            self.images.append(image)
-
-    def pixels_histogram(self, region=None, sum=None):
-        """
-        :param region (optional):
-            Region where to calculate histograms. Or (blc[0], blc[1], trc[0],
-            trc[1],) or (center[0], center[1], r, None,).
-        :param sum (optional):
-            Calculate sum of pixels in region?
-        """
-        # First, create mask
-        for i, image in enumerate(self.images):
-            self._3d_array[i] = image.data
-
-    def cross_correlate_with_image(self, image,
-                                   region1=(None, None, None, None),
-                                   region2=(None, None, None, None)):
-        """
-        Cross-correlate with one image.
-
-        :param image:
-            Instance of ``Image`` class.
-        :param region1:
-            Region to EXCLUDE in current instance images. Or (blc[0], blc[1],
-            trc[0], trc[1],) or (center[0], center[1], r, None,).
-        :param region2:
-            Region to EXCLUDE in ``image``. Or (blc[0], blc[1], trc[0], trc[1],)
-            or (center[0], center[1], r, None,).
-        :return:
-            Tuple of tuples (dx, dy,) of shifts (subpixeled) in each direction.
-        """
-        pass
-
-    def cross_correlate_with_set_of_images(self, images,
-                                           region1=(None, None, None, None),
-                                           region2=(None, None, None, None)):
-        """
-        Cross-correlate with set of images.
-
-        :param images:
-            Instance of ``ImageSet`` class.
-        :param region1:
-            Region to EXCLUDE in current instance images. Or (blc[0], blc[1],
-            trc[0], trc[1],) or (center[0], center[1], r, None,).
-        :param region2:
-            Region to EXCLUDE in images of ``images``. Or (blc[0], blc[1],
-            trc[0], trc[1],) or (center[0], center[1], r, None,).
-        :return:
-            Tuple of tuples (dx, dy,) of shifts (subpixeled) in each direction.
-        """
-        pass
+#class MemImage(Image, Model):
+#    """
+#    Class that represents image made using MEM algorithm.
+#    """
+#    pass

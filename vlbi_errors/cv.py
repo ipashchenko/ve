58601c6b1553c2a35bbf37c10ed3b784438e5f3e
Ilya Pashchenko <in4pashchenko@gmail.com>
2015-01-21 14:12:00 +0300

    added cv.py

--- vlbi_errors/cv.py
+++ vlbi_errors/cv.py
@@ -1,1 +1,63 @@
-__author__ = 'ilya'
+import glob
+from model import Model
+
+
+class CrossValidation(object):
+    """
+    Class that implements cross-validation analysis of image-plane models.
+    """
+    def __init__(self, data):
+        self.data = data
+
+    def run(self, modelcard=None, testcard=None, stokes='I'):
+        """
+        Method that cross-validates set of image-plane models obtained by
+        modelling training samples on corresponding set of testing samples.
+
+        :param modelfiles:
+            Wildcard of file names ~ 'model_0i_0jofN.txt', where model in
+            'model_0i_0jofN.txt' file is from modelling ``0j``-th training
+            sample ('train_0jofN.FITS') with ``0i``-th model.
+
+        :param testfiles:
+            Wildcard of file names ~ 'test_0jofN.FITS'.
+
+        :return:
+            List of lists [modelfilename, CV-score, sigma_cv_score].
+        """
+
+        modelfiles = glob.glob(modelcard)
+        testfiles = glob.glob(testcard)
+        modelfiles.sort()
+        testfiles.sort()
+        ntest = len(testfiles)
+        nmodels = len(modelfiles) / ntest
+
+        assert(not len(modelfiles) % float(len(testfiles)))
+
+        print "modelfiles : " + str(modelfiles)
+        print "testfiles : " + str(testfiles)
+
+        result = list()
+
+        for i in range(nmodels):
+            print "using models " + str(modelfiles[ntest * i: ntest * (i + 1)]) \
+                  + " and testing sample " + str(testfiles)
+            models = modelfiles[ntest * i: ntest * (i + 1)]
+            cv_scores = list()
+            for j, testfile in enumerate(testfiles):
+                model = Model()
+                model.add_from_txt(models[j], stoke=stokes)
+                print "using test file " + str(testfile)
+                data = open_fits(testfile)
+                cv_score = data.cv_score(model, stokes=stokes)
+                print "cv_score for one testing sample is " + str(cv_score)
+                cv_scores.append(cv_score)
+
+            mean_cv_score = np.mean(cv_scores)
+            std_cv_score = np.std(cv_scores)
+            print mean_cv_score, std_cv_score
+
+            result.append(["model#" + str(i + 1), mean_cv_score, std_cv_score])
+
+        return result

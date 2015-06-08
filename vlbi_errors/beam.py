a606482ef8f835d9221d4c788e175a765745c85f
Ilya Pashchenko <in4pashchenko@gmail.com>
2015-06-08 11:02:31 +0300

    usie # of pixs in CleanBeam.__init__

--- vlbi_errors/beam.py
+++ vlbi_errors/beam.py
@@ -12,7 +12,8 @@ def create_clean_beam_from_fits(fname):
     Create instance of ``CleanBeam`` from FITS-file of CLEAN image.
     """
     imsize, pixref, (bmaj, bmin, bpa), pixsize = get_fits_image_info(fname)
-    return CleanBeam(bmaj, bmin, bpa, imsize)
+    return CleanBeam(bmaj / abs(pixsize[0]), bmin / abs(pixsize[0]), bpa,
+                     imsize)
 
 
 class Beam(object):

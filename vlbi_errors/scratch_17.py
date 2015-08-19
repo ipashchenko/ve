f1d52340bc3e686abbbc7d13c78ceb8ce20017dd
Ilya Pashchenko <in4pashchenko@gmail.com>
2015-08-19 16:21:40 +0300

    seems to plot mas-ccordinates with write zero

--- vlbi_errors/scratch_17.py
+++ vlbi_errors/scratch_17.py
@@ -32,6 +32,16 @@ def gaussian(height, x0, y0, bmaj, e=0.3, bpa=0, size_x=100):
 
 if __name__ == '__main__':
 
+    # Plot every k-th pol.vector
+    k = 3
+    blc = (240, 200)
+    trc = (300, 350)
+    # Pixels
+    x_center = blc[1] + (trc[1] - blc[1]) / 2. - 256
+    y_center = blc[0] + (trc[0] - blc[0]) / 2. - 256
+    # Pixsels
+    x_slice = slice(blc[1], trc[1], None)
+    y_slice = slice(blc[0], trc[0],  None)
     # Create picture with contours - I, color - fpol and vectors - direction and
     # value of Linear Polarization
     import os
@@ -65,22 +75,30 @@ if __name__ == '__main__':
 
     beam_place = 'ul'
     pixsize = abs(i_image.pixsize[0])
-    imsize = i_image.imsize[0]
+    imsize_x = x_slice.stop - x_slice.start
+    imsize_y = y_slice.stop - y_slice.start
     factor = 206264806.719150
-    arc_length = pixsize * imsize * factor
-    x_, y_ = np.mgrid[-imsize/2: imsize/2, -imsize/2: imsize/2]
-    x_ *= pixsize * factor
-    y_ *= pixsize * factor
-    u = ppol_image.image * np.cos(pang_image.image)
-    v = ppol_image.image * np.sin(pang_image.image)
-    arc_length = pixsize * imsize * factor
-    x = y = np.linspace(-arc_length/2, arc_length/2, imsize)
-    i_array = i_image.image_w_residuals
-    ppol_array = ppol_image.image
-    fpol_array = fpol_image.image
+    # mas
+    arc_length_x = pixsize * imsize_x * factor
+    arc_length_y = pixsize * imsize_y * factor
+    # mas
+    x_ = i_image.x[0, :][x_slice] * factor
+    y_ = i_image.y[:, 0][y_slice] * factor
+    u = ppol_image.image[x_slice, y_slice] * np.cos(pang_image.image[x_slice,
+                                                    y_slice])
+    v = ppol_image.image[x_slice, y_slice] * np.sin(pang_image.image[x_slice,
+                                                                     y_slice])
+    # arc_length = pixsize * imsize * factor
+    # x = y = np.linspace(-arc_length/2, arc_length/2, imsize)
+    # FIXME: wrong zero location
+    x = np.linspace(x_[0], x_[-1], imsize_x)
+    y = np.linspace(y_[0], y_[-1], imsize_y)
+    i_array = i_image.image_w_residuals[x_slice, y_slice]
+    ppol_array = ppol_image.image[x_slice, y_slice]
+    fpol_array = fpol_image.image[x_slice, y_slice]
     # Creating masks
-    i_mask = np.zeros((imsize, imsize))
-    ppol_mask = np.zeros((imsize, imsize))
+    i_mask = np.zeros((imsize_x, imsize_y))
+    ppol_mask = np.zeros((imsize_x, imsize_y))
     # i_mask[abs(i_array) < 0.0001] = 1
     ppol_mask[ppol_array < 0.003] = 1
     fpol_mask = np.logical_or(i_mask, ppol_mask)
@@ -90,23 +108,22 @@ if __name__ == '__main__':
     ppol_array_masked = np.ma.array(ppol_array, mask=ppol_mask)
     fig = plt.figure()
     ax = fig.add_axes([0.1, 0.1, 0.6, 0.8])
+    # FIXME: wrong zero location
     i = ax.imshow(fpol_array_masked, interpolation='none', aspect='auto',
-                  label='FPOL', extent=[-arc_length/2, arc_length/2,
-                                        -arc_length/2, arc_length/2],
-                  origin='lower')
-    co = ax.contour(x, y, i_array_masked, colors='k', label='I')
+                  label='FPOL', extent=[y[0], y[-1], x[0], x[-1]], origin='lower')
+    co = ax.contour(y, x, i_array_masked, colors='k', label='I')
     m = np.zeros(u.shape)
     u = np.ma.array(u, mask=ppol_mask)
     v = np.ma.array(v, mask=ppol_mask)
-    vec = ax.quiver(x[::2], y[::2], u[::2, ::2], v[::2, ::2],
+    vec = ax.quiver(y[::k], x[::k], u[::k, ::k], v[::k, ::k],
                     angles='uv', units='xy', headwidth=0, headlength=0,
                     headaxislength=0, scale=0.005, width=0.05)
     # Doesn't show anything
     ax.legend()
     # c = Circle((5, 5), radius=4,
     #            edgecolor='red', facecolor='blue', alpha=0.5)
-    e_height = 10
-    e_width = 5
+    e_height = 10 * pixsize * factor
+    e_width = 5 * pixsize * factor
     r_min = e_height / 2
     if beam_place == 'lr':
         y_c = y[0] + r_min

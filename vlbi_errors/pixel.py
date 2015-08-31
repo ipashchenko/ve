9c357bf6f299f0f0d503af4a8388291a85b2fbd0
Ilya Pashchenko <in4pashchenko@gmail.com>
2015-08-31 15:17:14 +0300

    values for multimodal result

--- vlbi_errors/pixel.py
+++ vlbi_errors/pixel.py
@@ -1,23 +1,26 @@
 import numpy as np
 from pymc3 import (Model, Normal, Categorical, Dirichlet, Metropolis, sample,
-                   constant, ElemwiseCategoricalStep)
+                   constant, ElemwiseCategoricalStep, NUTS)
 
 
 # Create data
 # lambda squared [m^2]
 x = np.array([0.00126661, 0.00136888, 0.00359502, 0.00423771])
 # PANG [rad]
-y = np.array([-0.28306073, -0.21232782, -0.77439868,  2.08342187])
+y = np.array([-0.28306073, -0.21232782, -0.77439868,  0.75342187])
 # Uncertainties of PANG values [rad]
-sy = np.array([ 0.26500595,  0.29110131,  0.17655808,  0.13682663])
+sy = np.array([ 0.26500595,  0.29110131,  0.17655808,  0.44442663])
 
 with Model() as model:
     a = constant(np.array([1., 1., 1.]))
     alpha = Normal('alpha', mu=0., sd=np.pi)
-    beta = Normal('beta', mu=0., sd=300.)
+    beta = Normal('beta', mu=0., sd=500.)
     dd = Dirichlet('dd', a=a, shape=3)
-    j = Categorical('j', p=dd, shape=4)
+    j = Categorical('j', p=dd, shape=len(y))
 
+    # j=0 means data point should be lowered by pi
+    # j=1 means data point is ok
+    # j=2 means data point should be upped by pi
     mu = alpha + beta * x - np.pi * (j - 1)
 
     Y_obs = Normal('Y_obs', mu=mu, sd=sy, observed=y)

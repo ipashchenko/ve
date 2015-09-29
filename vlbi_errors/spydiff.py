80a0178bc6037c4f586ebf0ba3bcd1e2300dc4a6
Ilya Pashchenko <in4pashchenko@gmail.com>
2015-09-29 13:53:16 +0300

    moved import_difmap_model here

--- vlbi_errors/spydiff.py
+++ vlbi_errors/spydiff.py
@@ -1,7 +1,6 @@
 import os
 import numpy as np
 from components import DeltaComponent, CGComponent
-from utils import mas_to_rad
 
 
 # TODO: add ``shift`` argument, that shifts image before cleaning. It must be
@@ -73,16 +72,19 @@ def clean_difmap(fname, outfname, stokes, mapsize_clean, path=None,
     os.system("difmap < difmap_commands")
 
 
-def import_difmap_model(mdl_fname, model_dir):
+def import_difmap_model(mdl_fname, mdl_dir):
     """
     Function that reads difmap-format model and returns list of ``Components``
-    instances,
+    instances.
 
     :param mdl_fname:
-    :param model_dir:
+        File name with difmap model.
+    :param mdl_dir:
+        Directory with difmap model.
     :return:
+        List of ``Components`` instances.
     """
-    mdl = os.path.join(model_dir, mdl_fname)
+    mdl = os.path.join(mdl_dir, mdl_fname)
     mdlo = open(mdl, 'r')
     lines = mdlo.readlines()
     comps = list()
@@ -91,16 +93,17 @@ def import_difmap_model(mdl_fname, model_dir):
             continue
         line = line.strip('\n ')
         flux, radius, theta, major, axial, phi, type_, freq, spec = line.split()
-        x = float(radius[:-1]) * np.sin(np.deg2rad(float(theta[:-1])))
-        y = float(radius[:-1]) * np.sin(np.deg2rad(float(theta[:-1])))
+        x = -float(radius[:-1]) * np.sin(np.deg2rad(float(theta[:-1])))
+        y = -float(radius[:-1]) * np.cos(np.deg2rad(float(theta[:-1])))
         flux = float(flux[:-1])
         if int(type_) == 0:
-            comp = DeltaComponent(flux, mas_to_rad * x, mas_to_rad * y)
-        if int(type_) == 1:
-            bmaj = mas_to_rad * float(major[:-1])
-            comp = CGComponent(flux, mas_to_rad * x, mas_to_rad * y, bmaj)
+            comp = DeltaComponent(flux, x, y)
+        elif int(type_) == 1:
+            bmaj = float(major[:-1])
+            comp = CGComponent(flux, x, y, bmaj)
+        else:
+            raise NotImplementedError("Only CC and CG are implemented")
         comps.append(comp)
-
     return comps
 
 
@@ -144,89 +147,88 @@ def modelfit_difmap(fname, mdl_fname, out_fname, niter=50, stokes='i',
     difmapout.close()
     os.system("difmap < difmap_commands")
 
-
-## DIFMAP_MAPPSR
-#def difmap_mappsr(source, isll, centre_ra_deg, centre_dec_deg, uvweightstr,
-#                  experiment, difmappath, uvprefix, uvsuffix, jmsuffix, \
-#                  saveagain):
-#    difmapout = open("difmap_commands", "w")
-#    difmapout.write("float pkflux\n")
-#    difmapout.write("float peakx\n")
-#    difmapout.write("float peaky\n")
-#    difmapout.write("float finepeakx\n")
-#    difmapout.write("float finepeaky\n")
-#    difmapout.write("float rmsflux\n")
-#    difmapout.write("integer ilevs\n")
-#    difmapout.write("float lowlev\n")
-#    difmapout.write("obs " + sourceuvfile + "\n")
-#    difmapout.write("mapsize 1024," + str(pixsize) + "\n")
-#    difmapout.write("select ll,1,2,3,4\n")
-#    difmapout.write("invert\n");
-#    difmapout.write("wtscale " + str(threshold) + "\n")
-#    difmapout.write("obs " + sourceuvfile + "\n")
-#    if reweight:
-#        difmapout.write("uvaver 180,true\n")
-#    else:
-#        difmapout.write("uvaver 20\n")
-#    difmapout.write("mapcolor none\n")
-#    difmapout.write("uvweight " + uvweightstr + "\n")
+# # DIFMAP_MAPPSR
+# def difmap_mappsr(source, isll, centre_ra_deg, centre_dec_deg, uvweightstr,
+#                   experiment, difmappath, uvprefix, uvsuffix, jmsuffix, \
+#                   saveagain):
+#     difmapout = open("difmap_commands", "w")
+#     difmapout.write("float pkflux\n")
+#     difmapout.write("float peakx\n")
+#     difmapout.write("float peaky\n")
+#     difmapout.write("float finepeakx\n")
+#     difmapout.write("float finepeaky\n")
+#     difmapout.write("float rmsflux\n")
+#     difmapout.write("integer ilevs\n")
+#     difmapout.write("float lowlev\n")
+#     difmapout.write("obs " + sourceuvfile + "\n")
+#     difmapout.write("mapsize 1024," + str(pixsize) + "\n")
+#     difmapout.write("select ll,1,2,3,4\n")
+#     difmapout.write("invert\n");
+#     difmapout.write("wtscale " + str(threshold) + "\n")
+#     difmapout.write("obs " + sourceuvfile + "\n")
+#     if reweight:
+#         difmapout.write("uvaver 180,true\n")
+#     else:
+#         difmapout.write("uvaver 20\n")
+#     difmapout.write("mapcolor none\n")
+#     difmapout.write("uvweight " + uvweightstr + "\n")
 #
-#    #Find the peak from the combined first
-#    if isll:
-#        difmapout.write("select ll,1,2,3,4\n")
-#    else:
-#        difmapout.write("select i,1,2\n")
-#    if experiment == 'v190k':
-#        difmapout.write("select rr,1,2,3,4\n")
-#    difmapout.write("peakx = peak(x,max)\n")
-#    difmapout.write("peaky = peak(y,max)\n")
-#    difmapout.write("shift -peakx,-peaky\n")
-#    difmapout.write("mapsize 1024,0.1\n")
-#    difmapout.write("finepeakx = peak(x,max)\n")
-#    difmapout.write("finepeaky = peak(y,max)\n")
+#     #Find the peak from the combined first
+#     if isll:
+#         difmapout.write("select ll,1,2,3,4\n")
+#     else:
+#         difmapout.write("select i,1,2\n")
+#     if experiment == 'v190k':
+#         difmapout.write("select rr,1,2,3,4\n")
+#     difmapout.write("peakx = peak(x,max)\n")
+#     difmapout.write("peaky = peak(y,max)\n")
+#     difmapout.write("shift -peakx,-peaky\n")
+#     difmapout.write("mapsize 1024,0.1\n")
+#     difmapout.write("finepeakx = peak(x,max)\n")
+#     difmapout.write("finepeaky = peak(y,max)\n")
 #
-#    #Do each individual band, one at a time
-#    maxif = 4
-#    pols = ['rr','ll']
-#    for i in range(maxif):
-#        for p in pols:
-#            difmapout.write("select " + p + "," + str(i+1) + "\n")
-#            write_difmappsrscript(source, p + "." + str(i+1), difmapout, \
-#                                  pixsize, jmsuffix)
-#    write_difmappsrscript(source, 'combined', difmapout, \
-#                          pixsize, jmsuffix)
-#    if saveagain:
-#        difmapout.write("wobs " + rootdir + "/" + experiment + "/noise" + \
-#                        source + ".uvf\n")
-#    difmapout.write("exit\n")
-#    difmapout.close()
-#    os.system(difmappath + " < difmap_commands")
+#     #Do each individual band, one at a time
+#     maxif = 4
+#     pols = ['rr','ll']
+#     for i in range(maxif):
+#         for p in pols:
+#             difmapout.write("select " + p + "," + str(i+1) + "\n")
+#             write_difmappsrscript(source, p + "." + str(i+1), difmapout, \
+#                                   pixsize, jmsuffix)
+#     write_difmappsrscript(source, 'combined', difmapout, \
+#                           pixsize, jmsuffix)
+#     if saveagain:
+#         difmapout.write("wobs " + rootdir + "/" + experiment + "/noise" + \
+#                         source + ".uvf\n")
+#     difmapout.write("exit\n")
+#     difmapout.close()
+#     os.system(difmappath + " < difmap_commands")
 #
-## WRITE_DIFMAPPSRSCRIPT
-#def write_difmappsrscript(source, bands, difmapout, pixsize, jmsuffix):
-#    difmapout.write("clrmod true\n")
-#    difmapout.write("unshift\n")
-#    difmapout.write("shift -peakx,-peaky\n")
-#    difmapout.write("mapsize 1024,0.1\n")
-#    difmapout.write("pkflux = peak(flux,max)\n")
-#    difmapout.write("addcmp pkflux, true, finepeakx, finepeaky, true, 0, " + \
-#                    "false, 1, false, 0, false, 0, 0, 0\n")
-#    difmapout.write("mapsize 1024," + str(pixsize) + "\n")
-#    difmapout.write("modelfit 50\n")
-#    difmapout.write("rmsflux = imstat(rms)\n")
-#    difmapout.write("restore\n")
-#    difmapout.write("pkflux = peak(flux,max)\n")
-#    difmapout.write("ilevs = pkflux/rmsflux\n")
-#    difmapout.write("lowlev = 300.0/ilevs\n")
-#    difmapout.write("loglevs lowlev\n")
-#    imagefile = rootdir + '/' + experiment + '/' + source + '.' + bands + \
-#                '.image.fits' + jmsuffix
-#    if os.path.exists(imagefile):
-#        os.system("rm -f " + imagefile)
-#    difmapout.write("wmap " + imagefile + "\n")
-#    difmapout.write("unshift\n")
-#    difmapout.write("wmod\n")
-#    difmapout.write("print rmsflux\n")
+# # WRITE_DIFMAPPSRSCRIPT
+# def write_difmappsrscript(source, bands, difmapout, pixsize, jmsuffix):
+#     difmapout.write("clrmod true\n")
+#     difmapout.write("unshift\n")
+#     difmapout.write("shift -peakx,-peaky\n")
+#     difmapout.write("mapsize 1024,0.1\n")
+#     difmapout.write("pkflux = peak(flux,max)\n")
+#     difmapout.write("addcmp pkflux, true, finepeakx, finepeaky, true, 0, " + \
+#                     "false, 1, false, 0, false, 0, 0, 0\n")
+#     difmapout.write("mapsize 1024," + str(pixsize) + "\n")
+#     difmapout.write("modelfit 50\n")
+#     difmapout.write("rmsflux = imstat(rms)\n")
+#     difmapout.write("restore\n")
+#     difmapout.write("pkflux = peak(flux,max)\n")
+#     difmapout.write("ilevs = pkflux/rmsflux\n")
+#     difmapout.write("lowlev = 300.0/ilevs\n")
+#     difmapout.write("loglevs lowlev\n")
+#     imagefile = rootdir + '/' + experiment + '/' + source + '.' + bands + \
+#                 '.image.fits' + jmsuffix
+#     if os.path.exists(imagefile):
+#         os.system("rm -f " + imagefile)
+#     difmapout.write("wmap " + imagefile + "\n")
+#     difmapout.write("unshift\n")
+#     difmapout.write("wmod\n")
+#     difmapout.write("print rmsflux\n")
 #    difmapout.write("print imstat(bmin)\n")
 #    difmapout.write("print imstat(bmaj)\n")
 #    difmapout.write("print imstat(bpa)\n")

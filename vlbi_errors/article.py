06026b31c362a2c81b2168c9a1679553626e6f48
Ilya Pashchenko <in4pashchenko@gmail.com>
2016-06-21 21:34:53 +0300

    code for article

---
+++ vlbi_errors/article.py
@@ -0,0 +1,40 @@
+import numpy as np
+from itertools import combinations
+from sklearn import gaussian_process
+from from_fits import create_image_from_fits_file
+
+# Estimate correlation in image pixel values
+# FIXME: Better use residuals image from difmap or AIPS
+image_fits = '/home/ilya/code/learn/1226+023.j.2006_03_09.i_0.1.fits'
+image = create_image_from_fits_file(image_fits)
+
+slices = [slice(50 * i, 50 * (i+1)) for i in range(20)]
+sigma2_list = list()
+for slice1, slice2 in list(combinations(slices, 2))[:51]:
+    print "slices {} {}".format(slice1, slice2)
+    data = image.image[slice1, slice2]
+
+    X = list()
+    y = list()
+    for (i, j), val in np.ndenumerate(data):
+        X.append([i, j])
+        y.append(val)
+    Y = np.array(y).reshape(2500, 1)
+
+    gp = gaussian_process.GaussianProcess(thetaL=(0.01, 0.01),
+                                          thetaU=(100., 100.),
+                                          theta0=(1., 1.), nugget=0.0003**2,
+                                          storage_mode='full')
+    gpf = gp.fit(X, Y)
+    Y_pred = gpf.predict(X)
+    y_pred = Y_pred.reshape((50, 50))
+
+    fwhm = 2.355 * gpf.theta_
+    print "FWHM {}".format(fwhm)
+    # GP variance
+    sigma2 = gpf.sigma2
+    print "GP std {}".format(np.sqrt(sigma2))
+
+    sigma2_list.append((slice1, slice2, gpf.theta_))
+
+

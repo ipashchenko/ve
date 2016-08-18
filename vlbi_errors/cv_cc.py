5953d9e05fce281e9ecdeb2a55555362c1e95829
Ilya Pashchenko <in4pashchenko@gmail.com>
2016-08-18 20:51:29 +0300

    working creating test/train fits

--- vlbi_errors/cv_cc.py
+++ vlbi_errors/cv_cc.py
@@ -1,50 +1,54 @@
-import copy
 import numpy as np
 from uv_data import UVData
-from sklearn.cv import KFold
+from sklearn.cross_validation import KFold
 from spydiff import clean_difmap
 from from_fits import create_model_from_fits_file
 
 
+# TODO: Add optional ``rewrite`` argument to ``UVData.save``
 class KFoldCV(object):
     def __init__(self, fname, k, basename='cv'):
         self.fname = fname
         self.uvdata = UVData(fname)
         self.k = k
+        self.basename = basename
+        self.test_fname = "{}_test.FITS".format(basename)
+        self.train_fname = "{}_train.FITS".format(basename)
+        self.baseline_folds = None
         self.create_folds()
-        
+
     def create_folds(self):
         baseline_folds = dict()
-        for bl, indxs in self.uvdata._index_baselines.items():
-            kfold = KFold(np.nonzero(indx), self.k, shuffle=True)
+        for bl, indxs in self.uvdata._indxs_baselines.items():
+            kfold = KFold(np.count_nonzero(indxs), self.k, shuffle=True)
             baseline_folds[bl] = kfold
         self.baseline_folds = baseline_folds
-            
+
     def __iter__(self):
         for i in xrange(self.k):
             test_indxs = list()
             train_indxs = list()
             for bl, kfold in self.baseline_folds.items():
-                train, test = kfold[i]
-                train_indxs.extend(train)
-                test_indxs.extend(test)
-            train_data = self.uvdata[train]
-            test_data = self.uvdata[test]
-            self.uvdata.save('cv_train.fits', train_data)
-            self.uvdata.save('cv_test.fits', test_data)
-                    
-            yield train_fname, test_fname
-            
-           
+                itrain, itest = list(kfold)[i]
+                train_indxs.extend(itrain)
+                test_indxs.extend(itest)
+            train_data = self.uvdata.hdu.data[itrain]
+            test_data = self.uvdata.hdu.data[itest]
+            self.uvdata.save(self.test_fname, test_data, rewrite=True)
+            self.uvdata.save(self.train_fname, train_data, rewrite=True)
+
+            yield self.train_fname, self.test_fname
+
+
+seed = 42
+cc_pars = list()
 cv_scores = dict()
-for cc_par in cc_pars: 
+for cc_par in cc_pars:
     kfold = KFoldCV('some.fits', 50, seed)
     cv = list()
     for tr_fname, ts_fname in kfold:
-        clean_difmap(tr_fname, tr_model_fn, cc_par)
-        tr_model = create_model_from_fits_file(tr_model_fn)
+        clean_difmap(kfold.train_fname, 'trained_model.FITS', cc_par)
+        tr_model = create_model_from_fits_file('trained_model.FITS')
         ts_uvdata = UVData(ts_fname)
         cv.append(ts_uvdata.cv_score(tr_model))
     cv_scores[cc_par] = (np.mean(cv), np.std(cv))
-    
-    

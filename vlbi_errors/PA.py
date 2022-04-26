Massive update. Fix PA calculation. New functions in spydiff.

vlbi_errors/PA.py
@@ -56,5 +56,7 @@ def PA(JD, ra, dec, latitude, longitude):
 
     """
     LSTs = JD_to_LST(JD, longitude)
+    # HA will be in rad
     HA = LST_to_HA(LSTs, ra)
-    return np.arctan(np.sin(HA)/(np.tan(latitude) * np.cos(dec) - np.sin(dec) * np.cos(HA)))
+    return np.arctan2(np.sin(HA),
+                      np.tan(np.deg2rad(latitude)) * np.cos(np.deg2rad(dec)) - np.sin(np.deg2rad(dec)) * np.cos(HA))

vlbi_errors/bootstrap.py
@@ -306,7 +306,7 @@ def create_random_D_dict(uvdata, sigma_D):
     :return:
         Dictionary with keys [antenna name][integer of IF]["R"/"L"]
     """
-    import collections
+    from collections.abc import Mapping
     d_dict = dict()
     for ant in list(uvdata.antenna_mapping.values()):
         d_dict[ant] = dict()
@@ -314,7 +314,7 @@ def create_random_D_dict(uvdata, sigma_D):
             d_dict[ant][band] = dict()
             for pol in ("R", "L"):
                 # Generating two random complex numbers near (0, 0)
-                if isinstance(sigma_D, collections.Mapping):
+                if isinstance(sigma_D, Mapping):
                     rands = np.random.normal(loc=0, scale=sigma_D[ant], size=2)
                 else:
                     rands = np.random.normal(loc=0, scale=sigma_D, size=2)

vlbi_errors/image_ops.py
@@ -91,7 +91,8 @@ def add_dterm_evpa(q, u, i, evpa, d_term, n_ant, n_if, n_scans):
     return q_, u_
 
 
-def hovatta_find_sigma_pang(q, u, i, sigma_evpa, d_term, n_ant, n_if, n_scan):
+def hovatta_find_sigma_pang(q, u, i, sigma_evpa, d_term, n_ant, n_if, n_scan,
+                            check_equal_image_params=False):
     """
     Function that calculates uncertainty images of PANG & PPOL using (Hovatta et
     al. 2012) approach.
@@ -118,7 +119,8 @@ def hovatta_find_sigma_pang(q, u, i, sigma_evpa, d_term, n_ant, n_if, n_scan):
         uncertainties of PANG and second - with uncertainties of PPOL.
 
     """
-    assert q == u == i
+    if check_equal_image_params:
+        assert q == u == i
 
     # Create images of D-terms uncertainty
     i_peak = np.max(i.image.ravel())

vlbi_errors/utils.py
@@ -5,6 +5,8 @@ import re
 import math
 import numpy as np
 import astropy.io.fits as pf
+import astropy.units as u
+from astropy.modeling import models, fitting
 import matplotlib as mpl
 import string
 from math import floor
@@ -17,6 +19,7 @@ from sklearn import svm
 from sklearn.covariance import EllipticEnvelope, MinCovDet
 from sklearn.cluster import DBSCAN
 from sklearn.preprocessing import StandardScaler
+from skimage.registration._masked_phase_cross_correlation import cross_correlate_masked
 try:
     from skimage import transform
     have_skimage = True
@@ -40,10 +43,12 @@ vcomplex = np.vectorize(complex)
 v_int = np.vectorize(int)
 v_round = np.vectorize(round)
 
-mas_to_rad = 4.8481368 * 1E-09
-degree_to_rad = 0.01745329
-degree_to_mas = 36. * 10 ** 5
-
+deg2mas = u.deg.to(u.mas)
+deg2rad = u.deg.to(u.rad)
+mas2rad = u.mas.to(u.rad)
+degree_to_mas = deg2mas
+degree_to_rad = deg2rad
+mas_to_rad = mas2rad
 
 stokes_dict = {-8: 'YX', -7: 'XY', -6: 'YY', -5: 'XX',
                -4: 'LR', -3: 'RL', -2: 'LL', -1: 'RR',
@@ -1706,7 +1711,7 @@ def nested_dict_itervalue(nested):
             yield value
 
 
-def flatten(iterable, ltypes=collections.Iterable):
+def flatten(iterable, ltypes=collections.abc.Iterable):
     """
     http://stackoverflow.com/questions/2158395/flatten-an-irregular-list-of-lists-in-python/2158532#2158532
     :param iterable:
@@ -1776,3 +1781,139 @@ def transform_image(image, amplitude, shift_x, shift_y, scale_factor, rotation,
     tr_image *= amplitude
     tr_image[tr_image < cleaning_threshold] = 0.0
     return tr_image
+
+
+def get_significance_mask_from_mc(original_ccfits, mc_ccfits, mask=None, perc=2.5):
+    """
+    :param mask:
+        Pre-determined mask. Consider only unmasked pixesl.
+    """
+    from scipy.stats import percentileofscore
+    original = pf.getdata(original_ccfits).squeeze()
+    result = np.ones(original.shape, dtype=bool)
+    mc = [pf.getdata(ccfits).squeeze() for ccfits in mc_ccfits]
+    mc_cube = np.dstack(mc)
+    if mask is None:
+        mask = np.ones(original.shape, dtype=bool)
+    for (i, j), value in np.ndenumerate(original):
+        if mask[i, j]:
+            continue
+        low = percentileofscore(mc_cube[i, j, :] - np.median(mc_cube[i, j, :]) + original[i, j], 0.0)
+        if low < perc:
+            result[i, j] = False
+    return result
+
+
+def get_significance_mask_from_mc2(original_ccfits, mc_ccfits, mask=None, n_sigma_min=3):
+    """
+    :param mask:
+        Pre-determined mask. Consider only unmasked pixesl.
+    """
+    from scipy.stats import percentileofscore, norm
+    original = pf.getdata(original_ccfits).squeeze()
+    result = np.ones(original.shape, dtype=bool)
+    mc = [pf.getdata(ccfits).squeeze() for ccfits in mc_ccfits]
+    mc_cube = np.dstack(mc)
+    if mask is None:
+        mask = np.ones(original.shape, dtype=bool)
+    for (i, j), value in np.ndenumerate(original):
+        if mask[i, j]:
+            continue
+        sign = percentileofscore(mc_cube[i, j, :] - np.median(mc_cube[i, j, :]) + original[i, j], 0.0)/100.
+        n_sigma = norm.isf(sign)
+
+        if n_sigma > n_sigma_min or sign == 0.0:
+            result[i, j] = False
+
+    return result
+
+
+def get_errors_from_mc(mc_ccfits, mask=None):
+    from scipy.stats import scoreatpercentile
+    mc = [pf.getdata(ccfits).squeeze() for ccfits in mc_ccfits]
+    result = np.nan*np.ones(mc[0].shape, dtype=float)
+    mc_cube = np.dstack(mc)
+    if mask is None:
+        mask = np.ones(result.shape, dtype=bool)
+    for (i, j), value in np.ndenumerate(mc[0]):
+        if mask[i, j]:
+            continue
+        low, up = scoreatpercentile(mc_cube[i, j, :], [16, 84])
+        result[i, j] = 0.5*(up - low)
+    return result
+
+
+def get_beam_params_from_CCFITS(ccfits):
+    """
+    :return:
+        Beam parameters (bmaj[mas], bmin[mas], bpa[deg]).
+    """
+    bmaj, bmin, bpa = None, None, None
+    hdulist = pf.open(ccfits)
+    pr_header = hdulist[0].header
+    try:
+        # BEAM info in ``AIPS CG`` table
+        idx = hdulist.index_of('AIPS CG')
+        data = hdulist[idx].data
+        bmaj = float(data['BMAJ'])*deg2mas
+        bmin = float(data['BMIN'])*deg2mas
+        bpa = float(data['BPA'])
+    # In Petrov's data it in PrimaryHDU header
+    except KeyError:
+        try:
+            bmaj = pr_header['BMAJ']*deg2mas
+            bmin = pr_header['BMIN'] *deg2mas
+            bpa = pr_header['BPA']
+        except KeyError:
+            # In Denise data it is in PrimaryHDU ``HISTORY``
+            # TODO: Use ``pyfits.header._HeaderCommentaryCards`` interface if
+            # any
+            try:
+                for line in pr_header['HISTORY']:
+                    if 'BMAJ' in line and 'BMIN' in line and 'BPA' in line:
+                        bmaj = float(line.split()[3])*deg2mas
+                        bmin = float(line.split()[5])*deg2mas
+                        bpa = float(line.split()[7])
+            except KeyError:
+                pass
+        if not (bmaj and bmin and bpa):
+            raise Exception("Beam info absent!")
+
+    return bmin, bmaj, bpa
+
+
+def registrate_images(image1, image2, mask1, mask2, fit_gaussian=True, n=9):
+    """
+    :param image1:
+        2d numpy array with image.
+    :param image2:
+        2d numpy array with image.
+    :param mask1:
+        Must be ``True`` at valid pixels.
+    :param mask2:
+        Must be ``True`` at valid pixels.
+    :param fit_gaussian: (optional)
+        Fit 2D Gaussian to the peak of the correlation matrix?
+        (default: ``True``)
+    :param n: (optional)
+        Half-width [pix] of the square, centered on the position of the maximum
+        correlation, where to fit 2D Gaussian. (default: ``9``)
+    :return:
+        A tuple of shifts [pixels] (DEC, RA).
+    """
+    corr_matrix = cross_correlate_masked(image1, image2, mask1, mask2)
+    max_pos = np.unravel_index(np.argmax(corr_matrix), corr_matrix.shape)
+
+    if fit_gaussian:
+        # Grab a part pf image around maximal correlation coefficient
+        sub = corr_matrix[max_pos[0]-n:max_pos[0]+n, max_pos[1]-n:max_pos[1]+n]
+        x, y = np.mgrid[:2*n, :2*n]
+        p_init = models.Gaussian2D(1, n, n, n/2, n/2, 0)
+        fit_p = fitting.LevMarLSQFitter()
+        p = fit_p(p_init, x, y, sub)
+        # TODO: Check +1
+        result = p.x_mean.value-n+1, p.y_mean.value-n+1
+    else:
+        result = max_pos[0]-image1.shape[0]+1, max_pos[1]-image2.shape[1]+1
+
+    return result

vlbi_errors/uv_data.py
@@ -51,6 +51,32 @@ def downscale_uvdata_by_freq(uvdata):
     return downscale_by_freq
 
 
+def get_uvrange(uvfits, stokes=None):
+    """
+    :param stokes: (optional)
+        Stokes to consider masking. If ``None`` then ``I``. (default: ``None``)
+    :return:
+        Minimal and maximal uv-spacing in Ml.
+    """
+    uvdata = UVData(uvfits)
+    if stokes is None:
+        # Get RR and LL correlations
+        rrll = uvdata.uvdata_freq_averaged[:, :2]
+        # Masked array
+        I = np.ma.mean(rrll, axis=1)
+    else:
+        if stokes == "RR":
+            I = uvdata.uvdata_freq_averaged[:, 0]
+        elif stokes == "LL":
+            I = uvdata.uvdata_freq_averaged[:, 1]
+    weights_mask = I.mask
+
+    uv = uvdata.uv
+    uv = uv[~weights_mask]
+    r_uv = np.hypot(uv[:, 0], uv[:, 1])
+    return min(r_uv/10**6), max(r_uv/10**6)
+
+
 # FIXME: Handling FITS files with only one scan (used for CV)
 class UVData(object):
 
@@ -98,9 +124,13 @@ class UVData(object):
 
 
         rec = pf.getdata(self.fname, extname='AIPS AN')
+        # Sometimes from baselines we get non contigious list of antenna numbers
         # self._antenna_mapping = {number: rec['ANNAME'][i] for i, number in
         #                          enumerate(self.antennas)}
-        self._antenna_mapping = {antenna: rec['ANNAME'][antenna-1] for antenna in self.antennas}
+        try:
+            self._antenna_mapping = {antenna: rec['ANNAME'][antenna-1] for antenna in self.antennas}
+        except:
+            print("Failed to make antenna mappings. A bitchy AN-table!")
         self._antennas_baselines = None
         self._antennas_times = None
         self._minimal_antennas_time = None
@@ -837,6 +867,69 @@ class UVData(object):
                 self.uvdata[bl_indx, band, self.stokes_dict_inv["LR"]] =\
                     self.uvdata[bl_indx, band, self.stokes_dict_inv["LR"]] + add_LR
                 self.sync()
+
+
+    def add_D_nonlin(self, d_dict, imodel, qmodel, umodel):
+        """
+        Add D-terms contribution to data. Keep D^2 and D*P terms.
+
+        :param d_dict:
+            Dictionary with keys [antenna name][integer of IF]["R"/"L"]
+        :param imodel:
+            Instance of ``Model`` class for Stokes I.
+        :param qmodel:
+            Instance of ``Model`` class for Stokes Q.
+        :param umodel:
+            Instance of ``Model`` class for Stokes U.
+        """
+
+        from PA import PA
+        from utils import GRT_coordinates
+
+        models = list()
+        models.append(imodel)
+        models.append(qmodel)
+        models.append(umodel)
+        uvdata_copy = copy.deepcopy(self)
+        uvdata_copy.substitute(models)
+
+        for baseline in self.baselines:
+            bl_indx = self._get_baseline_indexes(baseline)
+            JD = self.times.jd[bl_indx]
+            ant1, ant2 = baselines_2_ants([baseline])
+            antname1 = self.antenna_mapping[ant1]
+            antname2 = self.antenna_mapping[ant2]
+            latitude1, longitude1 = GRT_coordinates[antname1]
+            latitude2, longitude2 = GRT_coordinates[antname2]
+            for band in range(self.nif):
+                d1R = d_dict[antname1][band]["R"]
+                d1L = d_dict[antname1][band]["L"]
+                d2R = d_dict[antname2][band]["R"]
+                d2L = d_dict[antname2][band]["L"]
+                I = 0.5*(uvdata_copy.uvdata[bl_indx, band, self.stokes_dict_inv["RR"]] +
+                         uvdata_copy.uvdata[bl_indx, band, self.stokes_dict_inv["LL"]])
+                LR = uvdata_copy.uvdata[bl_indx, band, self.stokes_dict_inv["LR"]]
+                RL = uvdata_copy.uvdata[bl_indx, band, self.stokes_dict_inv["RL"]]
+
+                pa1 = PA(JD, self.ra, self.dec, latitude1, longitude1)
+                pa2 = PA(JD, self.ra, self.dec, latitude2, longitude2)
+
+                # D_{1,R}*exp(+2i*fi_1)*I_{1,2} + D^*_{2,L}*exp(+2i*fi_2)*I_{1,2} --- for RL
+                # D_{1,L}*exp(-2i*fi_1)*I_{1,2} + D^*_{2,R}*exp(-2i*fi_2)*I_{1,2} --- for LR
+                # add_RL = I*(d1R*np.exp(2j*pa1) + d2L.conj()*np.exp(2j*pa2))
+                # add_LR = I*(d1L*np.exp(-2j*pa1) + d2R.conj()*np.exp(-2j*pa2))
+
+                add_RR = d1R*d2R.conj()*I*np.exp(1j*(+pa1-pa2)) + d1R*LR*np.exp(1j*(pa1+pa2)) + d2R.conj()*RL*np.exp(1j*(-pa1-pa2))
+                add_LL = d1L*d2L.conj()*I*np.exp(1j*(-pa1+pa2)) + d1L*RL*np.exp(1j*(-pa1-pa2)) + d2L.conj()*LR*np.exp(1j*(+pa1+pa2))
+                add_RL = I*(d1R*np.exp(1j*(pa1-pa2)) + d2L.conj()*np.exp(1j*(-pa1+pa2))) + d1R*d2L.conj()*LR*np.exp(1j*(+pa1+pa2))
+                add_LR = I*(d1L*np.exp(1j*(pa1+pa2)) + d2R.conj()*np.exp(1j*(pa1-pa2))) + d1L*d2R.conj()*RL*np.exp(1j*(-pa1-pa2))
+
+                self.uvdata[bl_indx, band, self.stokes_dict_inv["RR"]] = self.uvdata[bl_indx, band, self.stokes_dict_inv["RR"]] + add_RR
+                self.uvdata[bl_indx, band, self.stokes_dict_inv["LL"]] = self.uvdata[bl_indx, band, self.stokes_dict_inv["LL"]] + add_LL
+                self.uvdata[bl_indx, band, self.stokes_dict_inv["RL"]] = self.uvdata[bl_indx, band, self.stokes_dict_inv["RL"]] + add_RL
+                self.uvdata[bl_indx, band, self.stokes_dict_inv["LR"]] = self.uvdata[bl_indx, band, self.stokes_dict_inv["LR"]] + add_LR
+                self.sync()
+
 
     @property
     def scans(self):